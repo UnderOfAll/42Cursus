bf823286f9ea7dc8d369c902b927ebe6fbcff9b6
Kayki Eduardo Rocha Falass <karocha-@c1r6s9.42lisboa.com>
2024-11-05 17:11:19 +0000

    ps

---
+++ push_swap/push_swap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: karocha- <karocha-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/03 17:42:46 by karocha-          #+#    #+#             */
+/*   Updated: 2024/11/03 17:51:23 by karocha-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+#define PUSH_SWAP_H
+
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+
+#endif
