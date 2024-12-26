4a1094d81975d29a7e9effb7cba031e8ad993264
Kayki Eduardo Rocha Falass <karocha-@c1r3s8.42lisboa.com>
2024-12-26 21:37:05 +0000

    a

--- so_long/main.c
+++ so_long/main.c
@@ -6,13 +6,40 @@
 /*   By: karocha- <karocha-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 12:06:39 by karocha-          #+#    #+#             */
-/*   Updated: 2024/12/13 19:16:33 by karocha-         ###   ########.fr       */
+/*   Updated: 2024/12/26 20:15:06 by karocha-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+static int	check_arg(char **av)
+{
+	int	i;
+
+	if (!av)
+		return (0);
+	i = 0;
+	while (av[i])
+		i++;
+	if (av[i] == 'r' && av[i - 1] == 'e' && av[i - 2] == 'b' && av[i - 3] == '.')
+		return (1);
+	else
+		return (0);
+}
+
 int	main(int ac, char **av)
 {
-	if (ac != 2)
+	if (ac == 2)
+	{
+		if (check_arg)
+		{
+			
+		}
+	}
+	else
+	{
+		ft_printf("Error, invalid input\n");
+		exit (1);
+	}
+	return (0);
 }
--- so_long/so_long.h
+++ so_long/so_long.h
@@ -6,7 +6,7 @@
 /*   By: karocha- <karocha-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/11 12:06:42 by karocha-          #+#    #+#             */
-/*   Updated: 2024/12/13 19:34:15 by karocha-         ###   ########.fr       */
+/*   Updated: 2024/12/26 20:09:40 by karocha-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,30 @@
 
 # include "libraries/libft/libft.h"
 # include "libraries/minilibx/mlx.h"
+# include <fcntl.h>
 # include <limits.h>
 # include <stdint.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
+
+typedef struct
+{
+	char	**map;
+	void	*bakg;
+	void	*wall;
+	void	*player;
+	void	*collectable;
+	void	*exit;
+	int		map_x;
+	int		map_y;
+	int		img_x;
+	int		img_y;
+	int		player_x;
+	int		player_y;
+	int		moves;
+	int		endgame;
+}			t_game;
 
 #endif
